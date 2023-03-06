b6102fcf062dba3e8ad100ee7032dce232557c6d
srinivas <88233543+naresriv@users.noreply.github.com>
2023-03-06 23:56:31 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import CounterApp from './components/CounterApp/CounterApp';
+
+
+function App() {
+  return (
+    <div className="App">
+      <CounterApp/>
+    </div>
+  );
+}
+
+export default App;
